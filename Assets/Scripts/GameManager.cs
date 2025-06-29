d16212fa21f1fa56f613fee48b5c16b6c7bf4238
Juan Molinolo <juan.molinolo@innovant.us>
2025-06-29 01:01:47 -0300

    Basic pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,8 +3,14 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Image healthBar;
-    public float healthAmount = 100f;
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    [SerializeField]
+    private Image healthBar;
+
+    [SerializeField]
+    private float healthAmount = 100f;
 
     public void TakeDamage(float damage)
     {
@@ -17,5 +23,32 @@ public class GameManager : MonoBehaviour
         healthAmount += amount;
         if (healthAmount > 100f) healthAmount = 100f;
         healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf)
+            {
+                HidePauseMenu();
+            }
+            else
+            {
+                ShowPauseMenu();
+            }
+        }
+    }
+
+    private void ShowPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void HidePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
